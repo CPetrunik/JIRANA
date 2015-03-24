079791f2564b432161238b8684f7f8c933a1d57a
wongvk <wongvk@yahoo.com>
2015-03-24 14:17:17 -0700

    Changed Jira2 to query to fill in fields
    Updated query for Jira2 to fill in appropriate fields.

--- src/stud-dev.js
+++ src/stud-dev.js
@@ -172,16 +172,20 @@ $.getJSON(
             'https://jira2.workday.com/rest/api/2/search?jql=',
             'project%20%3D%20STU%20AND%20updated%20>%20"2014-11-24%2018%3A05"', //JQL REQUEST
             '&fields=',
-            'key,customfield_17400,status,summary,updated', // FIELDS
+            'key,customfield_10213,customfield_10306,assignee,issuetype,status,summary,updated', // FIELDS
             '&maxResults=3000'
         ].join(''),
         function (data) {
             $.each(data.issues, function (key, val) {
                 stud.d['j:' + val.key] = {
-                    'n': val.fields.summary
+                    'n': val.fields.summary,
+                    'u': val.fields.assignee.name,
+                    'IssueType': val.fields.issuetype.name,
+                    'VerifiedBy': val.fields.customfield_10213 != null ? val.fields.customfield_10213.name: null,
+                    'ToggleBy': val.fields.customfield_10306 != null ? val.fields.customfield_10306[0]: null
                 };
             });
-            //console.log(data);
+            console.log(data);
         });
 
 };
