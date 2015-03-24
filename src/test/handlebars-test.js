e00cbd6356b04bcb9586f9004db876679f0ea772
Ben McCurdy <bpmccurdy@gmail.com>
2015-03-23 19:43:36 -0700

    fix handlebars test files

---
+++ src/test/handlebars-test.js
@@ -0,0 +1,6 @@
+var source   = $("#entry-template").html();
+var template = Handlebars.compile(source);
+var context = {title: "My New Post", body: "This is my first post!"};
+var html    = template(context);   
+
+$('#template').html(html);
